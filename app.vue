91c5bf4c7e499057fda105838b88bbd26c96899b
Maksymilian Tomczyk <maksymilian.tomczyk@r.pl>
2022-07-06 14:28:34 +0200

    init

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    {{ cookie }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+  const { cookie } = useRequestHeaders(['cookie'])
+  console.log(cookie) // prints "undefined" for curl calls without cookie header
+</script>
